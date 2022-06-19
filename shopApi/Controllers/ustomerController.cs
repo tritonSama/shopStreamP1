d82b5dba4904db1d71bb07462dc619b4591013be
tritonSama <blue-aquarius@live.com>
2022-06-19 16:41:35 -0500

    update

--- shopApi/Controllers/ustomerController.cs
+++ shopApi/Controllers/ustomerController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using shopBL;
 using shopModel;
 
@@ -9,30 +10,76 @@ namespace shopApi.Controllers;
 public class CustomerController : ControllerBase
 {
     private iCustomerBL _custBL;
-    public CustomerController(iCustomerBL p_custBL)
+    private iCustItemJoinBL _custJoin;
+    public CustomerController(iCustomerBL p_custBL, iCustItemJoinBL custJoin)
     {
         _custBL = p_custBL;
+        _custJoin = custJoin;
     }
-    
-    
-    [HttpGet("GetAllItem")]
-    public IActionResult GetAllItem()
+
+
+    [HttpGet("GetAllCustomer")]
+    public IActionResult GetAllCustomer()
     {
+        try
+        {
         List<Customer> listOfCurrentCustomer = _custBL.GetAllCustomer();
         return Ok(listOfCurrentCustomer);
+        }
+        catch (SqlException)
+        {
+            return NotFound("Customer not found");
+        }
     } 
+
     
     
     [HttpPost("AddCustomer")]
     public IActionResult AddCustomer([FromBody]Customer p_cust)
     {
+        try
+        {
         _custBL.AddCustomer(p_cust);
         return Created("Customer Added", p_cust);
+        }
+        catch (System.Exception)
+        {
+            return Conflict();
+        }    
     }
 
     [HttpGet("SearchCustomerBy")]
     public IActionResult SearchCustomer([FromQuery] string custName)
     {
+        try
+        {
         return Ok(_custBL.searchCustomerByName(custName));
-    }  
+        }
+        catch (SqlException)
+        {
+            return Conflict();
+        }
+    }
+    [HttpPut("AdditemQuant")]
+    public IActionResult AddItemQuant([FromQuery] int p_quant, [FromQuery] int p_itemID, [FromQuery] int p_custID)
+    {
+        Customer found = _custBL.SearchCustomerByID(p_custID);
+
+        if (found == null)
+        {
+            return NotFound("Customer was not found");
+        }
+        else
+        {
+            try
+            {
+                _custJoin.AddItemQuant(p_quant,p_custID,p_itemID);
+                return Ok();
+            }
+            catch (SqlException)
+            {
+                return Conflict();
+            } 
+        }
+    }
 }
